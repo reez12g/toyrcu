819accf158fbff169a177c8b2b54fa0adceb12bf
reez12g <reez12g@gmail.com>
2023-04-17 22:12:23 +0900

    impl clone trait

--- src/rcu.rs
+++ src/rcu.rs
@@ -1,40 +1,59 @@
-use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
+use std::sync::{Arc, atomic::{AtomicUsize, AtomicPtr, Ordering}};
+use std::ptr::null_mut;
+use std::cell::RefCell;
 use std::ops::Deref;
 
 pub struct Rcu<T> {
     body: Arc<RcuBody<T>>,
 }
 
-pub struct RcuBody<T> {
-    data: T,
-    counter: AtomicUsize,
-}
+unsafe impl<T: Send + Sync> Send for Rcu<T> {}
+unsafe impl<T: Send + Sync> Sync for Rcu<T> {}
 
-impl<T> Rcu<T> {
-    pub fn new(data: T) -> Self {
+impl<T: Clone> Clone for Rcu<T> {
+    fn clone(&self) -> Self {
         Rcu {
-            body: Arc::new(RcuBody {
-                data,
-                counter: AtomicUsize::new(1),
-            }),
+            body: Arc::clone(&self.body),
         }
     }
 }
 
-impl<T> RcuBody<T> {
+impl<T> Deref for Rcu<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        let next = self.body.next.load(Ordering::Acquire);
+        if next == null_mut() {
+            return self.body.data.borrow().deref();
+        }
+        unsafe { &*next.cast::<T>() }
+    }
+}
+
+impl<'a, T: Clone> Rcu<T> {
+    pub fn new(data: T) -> Self {
+        Rcu {
+            body: Arc::new(RcuBody {
+                data: RefCell::new(data),
+                next: AtomicPtr::new(null_mut()),
+                counter: AtomicUsize::new(0),
+            }),
+        }
+    }
+
     // ReaderがRCUで保護されたデータへのReadを開始することを示す。
     pub fn read_lock(&self) {
-        self.counter.fetch_add(1, Ordering::Acquire);
+        self.body.counter.fetch_add(1, Ordering::Acquire);
     }
 
     // ReaderがCritical Sectionを抜けたことを示す。
     pub fn read_unlock(&self) {
-        self.counter.fetch_sub(1, Ordering::Release);
+        self.body.counter.fetch_sub(1, Ordering::Release);
     }
 
     // 全てのReaderがQuiescent Stateになるまで待機する。
     pub fn synchronize(&self) {
-        while self.counter.load(Ordering::Acquire) != 0 {
+        while self.body.counter.load(Ordering::Acquire) != 0 {
             core::hint::spin_loop();
         }
     }
@@ -45,14 +64,13 @@ impl<T> RcuBody<T> {
 
     // ReaderがRCUで保護されたデータ(ポインタ)を取得し、参照する。
     pub fn dereference(&self) -> &T {
-        &self.data
+        self.body.data.borrow().deref()
     }
+
 }
 
-impl<T> Deref for Rcu<T> {
-    type Target = RcuBody<T>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.body
-    }
+pub struct RcuBody<T> {
+    data: RefCell<T>,
+    next: AtomicPtr<T>,
+    counter: AtomicUsize,
 }
